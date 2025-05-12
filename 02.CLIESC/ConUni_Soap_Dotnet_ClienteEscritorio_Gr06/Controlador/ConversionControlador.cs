8cffca560ace8976bcdd56aa14ee74d5a1c9b243
Camila Morales <scmorales1@espe.edu.ec>
2025-05-11 21:16:06 -0500

    Cliente escritorio okay 💯

---
+++ 02.CLIESC/ConUni_Soap_Dotnet_ClienteEscritorio_Gr06/Controlador/ConversionControlador.cs
@@ -0,0 +1,32 @@
+﻿using ConUni_Soap_Dotnet_ClienteEscritorio_Gr06.ConversionServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConUni_Soap_Dotnet_ClienteEscritorio_Gr06.Controlador
+{
+    public class ConversionControlador
+    {
+        private readonly ConversiónClient modelo;
+        private Login loginForm;
+        private Form1 conversionForm;
+
+        public ConversionControlador(Login loginForm)
+        {
+            modelo = new ConversiónClient();
+            this.loginForm = loginForm;
+        }
+
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            return modelo.CelsiusToFahrenheit(celsius);
+        }
+
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            return modelo.FahrenheitToCelsius(fahrenheit);
+        }
+    }
+}
---
+++ 02.CLIESC/ConUni_Soap_Dotnet_ClienteEscritorio_Gr06/Modelo/ConversionModelo.cs
@@ -0,0 +1,29 @@
+﻿using ConUni_Soap_Dotnet_ClienteEscritorio_Gr06.ConversionServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConUni_Soap_Dotnet_ClienteEscritorio_Gr06.Modelo
+{
+    public class ConversionModelo
+    {
+        private ConversiónClient _client;
+
+        public ConversionModelo()
+        {
+            _client = new ConversiónClient();
+        }
+
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            return _client.CelsiusToFahrenheit(celsius);
+        }
+
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            return _client.FahrenheitToCelsius(fahrenheit);
+        }
+    }
+}
